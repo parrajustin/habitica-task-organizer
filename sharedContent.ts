25adc9bdf9b5e9e1719b39dc95b8005bfa3452c1
parrajustin <1360989+parrajustin@users.noreply.github.com>
2023-07-09 17:26:37 -0700

    Add dark theme - Add button on header to switch between color modes - Still have some issues to fix but is alright for now

--- sharedContent.ts
+++ sharedContent.ts
@@ -1,4 +1,5 @@
 import { Home } from "./index";
+import { Utils } from "./utils";
 
 export namespace SharedHtml {
   /**
@@ -10,4 +11,53 @@ export namespace SharedHtml {
     const routerPath = Home.GetPath();
     return routerPath.some ? routerPath.safeUnwrap() === path : false;
   }
+  /**
+   * Gets the current path.
+   * @returns string url of the current path
+   */
+  export function GetCurrentPath(): string {
+    const routerPath = Home.GetPath().unwrapOr<string>("default");
+    return Utils.GetScriptUrl(`?path=${routerPath}`);
+  }
+  /**
+   * Checks if currently in light mode.
+   * @returns true if user has light mode on
+   */
+  export function IsLightMode(): boolean {
+    const userProperties = PropertiesService.getUserProperties();
+    const modeSettings = userProperties.getProperty("COLOR_MODE") ?? "light";
+    return modeSettings === "light";
+  }
+  /**
+   * Gets the current color mode.
+   * @returns the color mode in string format
+   */
+  export function GetColorMode(): string {
+    const userProperties = PropertiesService.getUserProperties();
+    return userProperties.getProperty("COLOR_MODE") ?? "light";
+  }
+  /**
+   * Returns the opposite color mode.
+   * @returns "dark" if color mode is light
+   */
+  export function GetOppositeColorMode(): string {
+    const userProperties = PropertiesService.getUserProperties();
+    return (userProperties.getProperty("COLOR_MODE") ?? "light") === "light" ? "dark" : "light";
+  }
+}
+
+/**
+ * Changes the color mode of the user.
+ * @param color new color mode setting
+ * @returns true if succesfully set
+ */
+function ChangeColorMode(color: "light" | "dark"): boolean {
+  if (color !== "light" && color !== "dark") {
+    return false;
+  }
+
+  const userProperties = PropertiesService.getUserProperties();
+  userProperties.setProperty("COLOR_MODE", color === "light" ? "light" : "dark");
+
+  return true;
 }
